3291ab1f31270c4771cdee6aeea5d684da4c6b3e
Jason Johnson <jasonhjohnson@gmail.com>
2015-06-08 07:52:05 -0400

    Added twitter json

---
+++ web/app/themes/teamjb/page-twitter-json.php
@@ -0,0 +1,35 @@
+<?php
+header( 'Content-type: application/json' );
+ 
+/**
+ * Template Name: Cars JSON page
+ * Description: Outputs the list of cars as JSON
+ *
+ */
+ 
+$args = array(
+  'post_type' => 'florida-news',
+  'post_status' => 'publish',
+  'posts_per_page' => -1, // all
+  'orderby' => 'title',
+  'order' => 'ASC'
+);
+ 
+$query = new WP_Query( $args );
+
+$cars = array();
+ 
+while( $query->have_posts() ) : $query->the_post();
+ 
+  // Add a car entry
+  $cars[] = array(
+    'name' => get_the_title(),
+    'html' => get_the_content(),
+    'author' => get_the_author()
+  );
+ 
+endwhile;
+ 
+wp_reset_query();
+
+echo json_encode( $cars );
