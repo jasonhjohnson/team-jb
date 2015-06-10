921a89fe6d94035033499a6a7cbf0c1c275e0eea
Caleb Bell <antikathistemi@gmail.com>
2015-06-10 10:37:50 -0400

    * fixes prev/next styling for blog pages

--- web/app/themes/teamjb/page.php
+++ web/app/themes/teamjb/page.php
@@ -5,7 +5,7 @@ use Roots\Sage\Wrapper;
 
 ?>
 
-<div class="wrapper common coll-parallax" id="skrollr-body">
+<div class="wrapper common coll-parallax coll-single" id="skrollr-body">
   <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/content-header', basename(get_permalink())); ?>
     <?php get_template_part('templates/content-page', basename(get_permalink())); ?>
