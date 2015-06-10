85e34f8d5835597d9c11f21f1df57a0b0edb583f
Jason Johnson <jasonhjohnson@gmail.com>
2015-06-10 11:27:07 -0400

    Fixed some styles

--- web/app/themes/teamjb/page-calendar.php
+++ web/app/themes/teamjb/page-calendar.php
@@ -32,36 +32,18 @@
 </style>
 
 <div id="skrollr-body" role="main" class="wrapper common">	
-	
-    
-    <section id="team-list" class="coll-page-section type-coll-page-section status-publish hentry page-section ">
-      <div class="coll-section-background js-coll-parallax" style="background-color: #F1B950">
-        <div class="overlay" style=""></div>
-      </div>
-      <div class="section-content row ">
-        <div class="columns entry-title coll-hide-title">
-          <h2 class="title" style="color: #fff">Team ex2</h2>
-          <h4 class="subtitle" style="color: #fff"></h4>
+<section id="mgoodies" class="post-607 coll-page-section type-coll-page-section status-publish hentry page-section ">
+    <div class="coll-section-background js-coll-parallax"  style="background-color: #1a1a1a;min-height:800px;" >
+        <div class="overlay" style="min-height:800px;" ></div>
+            
         </div>
-        <div class="entry-content columns">
-          <div class="js-coll-smart-padding" data-coll-padding='{"max":10,"min":0}'></div>
-          <p>
-            <div class="row ">
-              <div class="columns large-12 medium-12 ">
-                <div class=" coll-text type-04 js-coll-texttype-resize" data-coll-font-size='{"max":120,"min":80}' style="color:#ffffff;border-color:#ffffff">
-                  <span class="text">Calendar</span>
-                </div>
-                <div class=" coll-text type-03 js-coll-texttype-resize" data-coll-font-size='{"max":40,"min":26}' style="color:#ffffff;border-color:#ffffff">
-                  <span class="text">of Events</span>
-                </div>
-              </div>
+        <div class="section-content row ">
+            <div class="columns entry-title coll-hide-title" >
+                <h2 class="title" style="color: #fff">MGoodies</h2>
+                    <h4 class="subtitle" style="color: #fff"></h4>
             </div>
-            <br /> &nbsp;
-            <br />
-            <div class="row">
-              <div class="columns large-12 medium-12" style="min-height:1000px">  
-                  
-                  <div class="custom-calendar-wrap custom-calendar-full">
+            <div class="entry-content columns" style="min-height:800px;">     
+                <div class="custom-calendar-wrap custom-calendar-full">
         				<div class="custom-header clearfix">        					
         					<h3 class="custom-month-year">
         						<span id="custom-month" class="custom-month"></span>
@@ -75,16 +57,10 @@
         				</div>
         				<div id="calendar" class="fc-calendar-container"></div>
         			</div>
-                    
-                <?php //echo do_shortcode ('[gcal id="13416"]'); ?>
-                <!--<div class="responsive-iframe-container big-container">
-              <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=jasonhjohnson%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FNew_York" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
-                  </div>-->
-              </div>
+                
+                <p></p>
             </div>
-          </p>
         </div>
-      </div>
     </section>      
 
     <footer class="site-footer">
