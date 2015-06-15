1e6777fd3b75291f66a4ddf2c052ecfe2fc5aeaf
Jason Johnson <jasonhjohnson@gmail.com>
2015-06-15 08:37:01 -0400

    Calendar credits

--- web/app/themes/teamjb/lib/utils.php
+++ web/app/themes/teamjb/lib/utils.php
@@ -12,25 +12,46 @@ function get_search_form() {
 }
 add_filter('get_search_form', __NAMESPACE__ . '\\get_search_form');
 
-function get_random_header_image() {
+function get_random_header_image($type = '') {
   // Selects at random from a collection of photos.  
   $photo = rand(1, 3);
   
   switch ($photo) {
     case 1:
-      $credit = "https://www.flickr.com/photos/three_if_by_bike/3428149800";
+      if ($type != "calendar") {
+        $credit = "https://www.flickr.com/photos/three_if_by_bike/3428149800";
+        $url = "/app/themes/teamjb/assets/images/page-headers/page-header-" . $photo . ".jpg";
+      }
+      else {
+        $credit = "https://www.flickr.com/photos/boston_public_library/8102637908/";
+        $url = "/app/themes/teamjb/assets/images/page-headers/calendar-header-" . $photo . ".jpg";
+      }
       break;
     
     case 2:
-      $credit = "https://www.flickr.com/photos/flguardian2/6849866709";
+    if ($type != 'calendar') {
+        $credit = "https://www.flickr.com/photos/flguardian2/6849866709";
+        $url = "/app/themes/teamjb/assets/images/page-headers/page-header-" . $photo . ".jpg";
+      }
+      else {
+        $credit = "https://www.flickr.com/photos/flguardian2/6860055869";
+        $url = "/app/themes/teamjb/assets/images/page-headers/calendar-header-" . $photo . ".jpg";
+      }
+      
       break;
     
     case 3: 
-      $credit = "https://www.flickr.com/photos/the_lylipad/6497519217";
+      if ($type != 'calendar') {
+        $credit = "https://www.flickr.com/photos/the_lylipad/6497519217";
+        $url = "/app/themes/teamjb/assets/images/page-headers/page-header-" . $photo . ".jpg";
+      }
+      else {
+        $credit = "https://www.flickr.com/photos/flguardian2/6859603355";
+        $url = "/app/themes/teamjb/assets/images/page-headers/calendar-header-" . $photo . ".jpg";
+      }
+      
       break;
-  }    
-  
-	$url = "/app/themes/teamjb/assets/images/page-headers/page-header-" . $photo . ".jpg";
+  }      
   
   return array(
     'url' => $url,
